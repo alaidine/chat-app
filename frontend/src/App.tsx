9f72e807c5a3f3089ae4eb4acf259b60f5e540e7
alaidine <aa.alimlinde@gmail.com>
2023-07-05 22:52:26 +0400

    you can now send message from the client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [message, setMessage] = useState("")
+
+  function sendMessage() {
+    axios.post('http://localhost:3000/messages/post_message', {value: message}).then((response) => console.log(response.data))
+    setMessage("")
+  }
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/messages/get_messages').then((response) => console.log(response.data))
+  }, [])
+
   return (
-    <>
-    </>
-    
+    <div className="wrapper">
+
+      <div>
+        <input onChange={(e) => setMessage(e.target.value)} placeholder='send a message' />
+        <button onClick={sendMessage} />
+      </div>
+    </div>
   )
 }
 
